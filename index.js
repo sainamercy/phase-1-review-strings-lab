720981239cb1566151ba774b9f2a509e6556cd8b
sainamercy <sainamercy@gmail.com>
2022-11-21 14:55:13 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = "Mercy Saina"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toUpperCase()
+
+const firstInitial = currentUser[0]
+const restOfName = currentUser.slice(1)
+
+const shortGreeting = `Welcome, ${firstInitial}!`
+
